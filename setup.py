b97e760cccfed24889f8ebdd0c3e31bf8a7ee00d
Hajime Nakagami <nakagami@gmail.com>
2020-08-28 23:12:39 +0900

    revision number up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pure-pyawabi",
-    version="0.1.0",
+    version="0.1.1",
     description='A morphological analyzer awabi clone',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
